8c1b014d7cbea12e1a9e2c00716ce2991e6b29a0
Mike Chen <stupidchen@foxmail.com>
2018-07-08 16:29:42 +0800

    Finished: Dockerfile.

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     # return "Welcome to 306 Studio Page!"
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=6666)
